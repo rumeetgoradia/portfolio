47b65b3e712736348bc615b3e295a2c542029537
Rumeet Goradia <rumeet.goradia@gmail.com>
2020-07-11 21:03:27 -0400

    Fix casing

---
+++ src/styles/Themes.js
@@ -0,0 +1,25 @@
+export const lightTheme = {
+  gradient: "linear-gradient(to bottom right, #dddde1, #e7e7ea, #f1f1f3)",
+  font: "#111820",
+  fontRGBA: "rgba(17, 24, 32, ",
+  body: "#e7e7ea",
+  bodyRGBA: "rgba(231, 231, 234, ",
+  navLinkShadow: "0 0 1px #111820",
+  containerColor: "rgba(227, 227, 229, .65)",
+  containerShadow: "2px 2px 6px rgba(17, 24, 32, 0.2);",
+  imgBorder: "transparent",
+  cardColor: "rgba(223, 223, 224, .45)",
+}
+
+export const darkTheme = {
+  gradient: "linear-gradient(to bottom right, #111820, #0e131a, #0a0e13)",
+  font: "#e7e7ea",
+  fontRGBA: "rgba(231, 231, 234, ",
+  body: "#111820",
+  bodyRGBA: "rgba(17, 24, 32, ",
+  navLinkShadow: "0 0 3px #f1f1f3",
+  containerColor: "rgba(19,26,35, .65)",
+  containerShadow: "none",
+  imgBorder: "#e7e7ea",
+  cardColor: "rgba(31, 41, 54,.35)",
+}
