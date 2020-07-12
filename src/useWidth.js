15250a46ba57aea6600cf6f2f4a87bc88656bd73
Rumeet Goradia <rumeet.goradia@gmail.com>
2020-07-11 22:11:25 -0400

    Fix useWidth initialization

--- src/useWidth.js
+++ src/useWidth.js
@@ -1,18 +1,23 @@
 import { useEffect, useState } from "react"
 
 export default function useWidth() {
+  const [hasRan, setHasRan] = useState(false)
   const [width, setWidth] = useState(0)
-
-  useEffect(() => {
-    window.addEventListener("resize", handleResize)
-    return () => {
-      window.removeEventListener("resize", handleResize)
-    }
-  }, [])
 
   const handleResize = () => {
     setWidth(window.innerWidth)
   }
+
+  useEffect(() => {
+    if (!hasRan) {
+      setHasRan(true)
+      handleResize()
+    }
+    window.addEventListener("resize", handleResize)
+    return () => {
+      window.removeEventListener("resize", handleResize)
+    }
+  }, [width])
 
   return width
 }
