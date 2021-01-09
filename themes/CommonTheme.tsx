d685cbf3bcac9a6e1cf82d5dd7ca4c85314597a5
Rumeet Goradia <rumeet.goradia@gmail.com>
2021-01-08 23:20:12 -0500

    Change error color

--- themes/CommonTheme.tsx
+++ themes/CommonTheme.tsx
@@ -13,7 +13,7 @@ export interface PortfolioTheme extends Theme {
 export const PRIMARY_COLOR: string = "rgb(52, 146, 110)"
 export const BLACK: string = "rgb(17, 24, 32)"
 export const WHITE: string = "rgb(231, 231, 234)"
-export const ERROR: string = "rgb(187, 0, 0)"
+export const ERROR: string = "rgb(194, 26, 26)"
 
 export const commonPaletteOptions: PaletteOptions = {
 	common: {
