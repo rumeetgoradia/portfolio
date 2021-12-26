09a3aad20f07368765bbba009a691db1f85851ac
Rumeet Goradia <rumeet.goradia@gmail.com>
2021-12-26 15:36:14 -0500

    Add favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,7 +13,36 @@ class MyDocument extends Document {
 						type="font/woff2"
 						crossOrigin="anonymous"
 					/>
-					{/* TODO: Add favicon info */}
+					<link
+						rel="apple-touch-icon"
+						sizes="180x180"
+						href="/favicons/apple-touch-icon.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="32x32"
+						href="/favicons/favicon-32x32.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="16x16"
+						href="/favicons/favicon-16x16.png"
+					/>
+					<link rel="manifest" href="/favicons/site.webmanifest" />
+					<link
+						rel="mask-icon"
+						href="/favicons/safari-pinned-tab.svg"
+						color="#035841"
+					/>
+					<link rel="shortcut icon" href="/favicons/favicon.ico" />
+					<meta name="msapplication-TileColor" content="#00a300" />
+					<meta
+						name="msapplication-config"
+						content="/favicons/browserconfig.xml"
+					/>
+					<meta name="theme-color" content="#ffffff" />
 				</Head>
 				<body>
 					<Main />
