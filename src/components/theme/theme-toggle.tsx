64c713ff7dd297db04c7656a8765521e1be2ac5b
Rumeet Goradia <rumeet.goradia@gmail.com>
2025-08-07 21:49:57 -0400

    Add theme and colors

---
+++ src/components/theme/theme-toggle.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import * as React from "react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+
+import { Button } from "~/components/ui/button";
+
+export function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <Button
+      variant="outline"
+      size="icon"
+      className="border-foreground/40 text-foreground/40 transition-colors hover:border-foreground hover:text-foreground"
+      onClick={(e) => {
+        e.preventDefault();
+        setTheme(theme === "dark" ? "light" : "dark");
+      }}
+    >
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 dark:rotate-0 dark:scale-100" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
